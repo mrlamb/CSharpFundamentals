7455e5df5bd8c42723899cf27f2c2de4b0bf3c9c
MRLamb <lamb.matthew@gmail.com>
2016-09-12 21:07:11 -0500

    Initial commit, first two practice programs

---
+++ Mario/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/***********************************
+ * Mario-1
+ * In the original Super Mario Brothers game at the end of nearly every level
+ * Mario would scale a pyramid and jump from the top to reach a flagpole.
+ *
+ * Your mission is to write a program to simulate the construction of that pyramid 
+ * using hashtags (#) for the blocks. 
+ *
+ * Later you will get input from the user to determine exactly how high the pyramid
+ * should be built but for now, assume a height of 10.
+ * 
+ * Lastly, the top of the pyramid should extend for two columns an example is shown below.
+ * Height 4 pyramid:
+ *
+ *     ##
+ *    ###
+ *   ####
+ *  #####
+ */
+
+namespace Mario
+{
+    class Mario1
+    {
+        static void Main(string[] args)
+        {
+            // You MAY NOT simply use 10 Console.WriteLine statements to build this!
+            // Think of another way to build the pyramid.
+            // TODO: Build Mario a pyramid!
+        }
+    }
+}
---
+++ Mario2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Utils.Utils;
+/***********************************
+ * Mario-2
+ * In the original Super Mario Brothers game at the end of nearly every level
+ * Mario would scale a pyramid and jump from the top to reach a flagpole.
+ *
+ * Your mission is to write a program to simulate the construction of that pyramid 
+ * using hashtags (#) for the blocks. 
+ *
+ * Further you will query the user for how tall the pyramid should be. Your input should look
+ * like this:
+ *
+ * Height? (user input goes here)
+ * 
+ * You will then build the pyramid using the input number. I have included a function to help
+ * ease into this aspect of the program. You may use the GetInt() method to retrieve an integer 
+ * from the user. This method handles error checking for you and is just meant to abstract that 
+ * portion back temporarily.
+ * 
+ * Lastly, the top of the pyramid should extend for two columns an example is shown below.
+ * Height 4 pyramid:
+ *
+ *     ##
+ *    ###
+ *   ####
+ *  #####
+ */
+namespace Mario
+{
+    class Mario2
+    {
+        static void Main(string[] args)
+        {
+            // An example of using the GetInt() method is like this
+            // int num = GetInt();
+            // Use it as appropriate based on your program requirements.
+        }
+    }
+}
